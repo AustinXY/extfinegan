96bf866c0863a3f98a4b7cb267f84e541895ffa6
AustinXY <ayxue@ucdavis.edu>
2019-10-09 09:00:00 -0700

    update

--- code/trainer.py
+++ code/trainer.py
@@ -696,7 +696,7 @@ class FineGAN_evaluator(object):
             # background_class = cfg.TEST_BACKGROUND_CLASS
             # parent_class = cfg.TEST_PARENT_CLASS
             # child_class = cfg.TEST_CHILD_CLASS
-            torch.manual_seed(7)
+            torch.manual_seed(8)
             bg_li = torch.randint(cfg.FINE_GRAINED_CATEGORIES, (self.batch_size,))
             p_li = torch.randint(cfg.SUPER_CATEGORIES, (self.batch_size,))
             c_li = torch.randint(cfg.FINE_GRAINED_CATEGORIES, (self.batch_size,))
