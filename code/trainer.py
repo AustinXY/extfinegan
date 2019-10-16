d7948c575b1ad8a9228a7c96f9c1700f62ee7837
AustinXY <ayxue@ucdavis.edu>
2019-10-15 20:28:06 -0700

    u

--- code/trainer.py
+++ code/trainer.py
@@ -384,7 +384,7 @@ class FineGAN_trainer(object):
 
                 # Sparsity loss
                 pti_concentration_loss = []
-                weight = 100
+                weight = 1e-3
                 for pt in range(cfg.NUM_PARTS):
                     errG_concentration = 0
                     for ix in range(batch_size):
