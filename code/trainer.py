29dff39e73b021d78c161619da9793176bea62de
AustinXY <ayxue@ucdavis.edu>
2019-11-08 07:15:27 -0800

    u

--- code/trainer.py
+++ code/trainer.py
@@ -454,14 +454,14 @@ class FineGAN_trainer(object):
                     # summary_D_class = summary.scalar('Sparsity_loss', errG_sparsity.data[0])
                     # self.summary_writer.add_summary(summary_D_class, count)
 
-                    summary_D_class = summary.scalar('Part_Concentraion_loss', errG_concentration.data[0])
-                    self.summary_writer.add_summary(summary_D_class, count)
+                    # summary_D_class = summary.scalar('Part_Concentraion_loss', errG_concentration.data[0])
+                    # self.summary_writer.add_summary(summary_D_class, count)
 
-                    summary_D_class = summary.scalar('Part_Separation_loss', errG_separation.data[0])
-                    self.summary_writer.add_summary(summary_D_class, count)
+                    # summary_D_class = summary.scalar('Part_Separation_loss', errG_separation.data[0])
+                    # self.summary_writer.add_summary(summary_D_class, count)
 
-                    summary_D_class = summary.scalar('Parent_child_masks_similarity_loss', errG_pmk_simloss.data[0])
-                    self.summary_writer.add_summary(summary_D_class, count)
+                    # summary_D_class = summary.scalar('Parent_child_masks_similarity_loss', errG_pmk_simloss.data[0])
+                    # self.summary_writer.add_summary(summary_D_class, count)
 
                 if i == 4:
                     summary_D_class = summary.scalar('mask_incomplete_loss', errG_incomplete.data[0])
