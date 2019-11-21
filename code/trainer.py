d597209260acfe9f55b7c2bffee18f0ef05c3af4
AustinXY <ayxue@ucdavis.edu>
2019-11-21 10:51:09 -0800

    u

--- code/trainer.py
+++ code/trainer.py
@@ -468,26 +468,6 @@ class FineGAN_trainer(object):
                 # errG_overlap = errG_overlap * weight
                 # errG_total = errG_total + errG_overlap
 
-                # # pixel with greater than threshold intensity will be considered as activated
-                # activated_threshold = 1e-2
-                # contributed_threshold = 1e-3
-                # parent_mask = self.mk_imgs[0]
-                # ones = torch.ones_like(parent_mask).cuda()
-                # zeros = torch.zeros_like(parent_mask).cuda()
-                # parent_bitmask = torch.where(parent_mask > activated_threshold, ones, zeros).cuda()
-                # running_mul = parent_bitmask.clone()
-                # for pt in range(cfg.NUM_PARTS):
-                #     # percentage of contribution of each pixel of part mask (want each pixel to contribute 100%)
-                #     _contribution_map = torch.div(self.c_mk[pt], parent_mask+self.protect_value)
-                #     contribution_map = torch.mul(parent_bitmask, _contribution_map)
-                #     _cont_mul = torch.mul(running_mul, contribution_map)
-                #     _uncont_mul = torch.mul(running_mul, contributed_threshold)
-                #     running_mul = torch.where(
-                #         contribution_map > contributed_threshold, _cont_mul, _uncont_mul)
-
-                # errG_overlap = torch.sum(running_mul) * weight
-                # errG_total = errG_total + errG_overlap
-
             # mask incomplete loss
             elif i == 4:
                 weight = 10
